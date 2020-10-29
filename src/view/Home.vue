10a2cd3a2c430a2a1208ad6a29ee6135037fd8b9
tang <923233700@qq.com>
2020-10-29 18:25:45 +0800

    封装 Topnav

--- src/view/Home.vue
+++ src/view/Home.vue
@@ -1,9 +1,6 @@
 <template>
   <div>
-    <div class="topnav">
-      <div class="logo"></div>
-      <div class="menu"></div>
-    </div>
+    <Topnav />
     <div class="banner">
       <h1>轱辘UI</h1>
       <h2>一个厉害的UI框架</h2>
@@ -16,7 +13,12 @@
 </template>
 
 <script>
-export default {};
+import Topnav from "../components/Topnav.vue";
+export default {
+  components: {
+    Topnav,
+  },
+};
 </script>
 
 <style lang="scss" scoped>
