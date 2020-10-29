5a0a0506dcdaa26deb2d5980991787e5b3ef195f
tang <923233700@qq.com>
2020-10-29 22:57:36 +0800

    路由切换后关闭 aside

---
+++ src/components/SwitchDemo.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>Switch 的文档</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+</style>
--- src/view/Doc.vue
+++ src/view/Doc.vue
@@ -6,7 +6,7 @@
         <h2>组件列表</h2>
         <ol>
           <li>
-            <router-link to="/doc/swich">Swich 组件</router-link>
+            <router-link to="/doc/switch">Switch 组件</router-link>
           </li>
           <li>
             <router-link to="/doc/button">Button 组件</router-link>
@@ -19,7 +19,9 @@
           </li>
         </ol>
       </aside>
-      <main>主内容</main>
+      <main>
+        <router-view />
+      </main>
     </div>
   </div>
 </template>
@@ -44,10 +46,7 @@ aside {
   background: lightblue;
   width: 150px;
   padding: 16px;
-  position: fixed;
-  padding-top: 70px;
-  top: 0;
-  left: 0;
+
   > h2 {
     margin-bottom: 4px;
   }
@@ -55,6 +54,12 @@ aside {
     > li {
       padding: 4px 0;
     }
+  }
+  @media (max-width: 500px) {
+    position: fixed;
+    padding-top: 70px;
+    top: 0;
+    left: 0;
   }
 }
 </style>
