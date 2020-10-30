22a001d2e15e3a7954ea580b6b34c88e18295726
tang <923233700@qq.com>
2020-10-30 19:55:09 +0800

    支持 theme 属性

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,4 +1,13 @@
 <template>
+  <div>Button 示例</div>
+  <h1>示例1</h1>
+  <div>
+    <Button>你好</Button>
+    <Button theme="button">你好</Button>
+    <Button theme="link">你好</Button>
+    <Button theme="text">你好</Button>
+  </div>
+  <h1>示例1</h1>
   <div>
     <Button>你好</Button>
     <Button theme="button">你好</Button>
