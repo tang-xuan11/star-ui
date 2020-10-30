03be406049691cf6a638329519d223851e551c4c
tang <923233700@qq.com>
2020-10-30 20:09:26 +0800

    支持 size 属性

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -9,10 +9,9 @@
   </div>
   <h1>示例1</h1>
   <div>
-    <Button>你好</Button>
-    <Button theme="button">你好</Button>
-    <Button theme="link">你好</Button>
-    <Button theme="text">你好</Button>
+    <Button size="big">大按钮</Button>
+    <Button>默认按钮</Button>
+    <Button size="small">小按钮</Button>
   </div>
 </template>
 
