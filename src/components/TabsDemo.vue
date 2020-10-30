55531f4f98f8b8bfb88f430472a6cf5f6e184c02
tang <923233700@qq.com>
2020-10-30 09:35:30 +0800

    基本完成官网

---
+++ src/components/TabsDemo.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>Tabs 的文档</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+</style>
--- src/router.ts
+++ src/router.ts
@@ -2,6 +2,10 @@ import { createWebHashHistory, createRouter } from "vue-router";
 import Home from "./view/Home.vue";
 import Doc from "./view/Doc.vue";
 import SwitchDemo from "./components/SwitchDemo.vue";
+import ButtonDemo from "./components/ButtonDemo.vue";
+import DialogDemo from "./components/DialogDemo.vue";
+import TabsDemo from "./components/TabsDemo.vue";
+import DocDemo from "./components/DocDemo.vue";
 
 const history = createWebHashHistory();
 export const router = createRouter({
@@ -16,8 +20,24 @@ export const router = createRouter({
       component: Doc,
       children: [
         {
+          path: "",
+          component: DocDemo,
+        },
+        {
           path: "switch",
           component: SwitchDemo,
+        },
+        {
+          path: "button",
+          component: ButtonDemo,
+        },
+        {
+          path: "dialog",
+          component: DialogDemo,
+        },
+        {
+          path: "tabs",
+          component: TabsDemo,
         },
       ],
     },
