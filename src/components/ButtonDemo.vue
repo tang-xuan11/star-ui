579a81a85c9a0994b44f43d1adb9a0197b804258
tang <923233700@qq.com>
2020-10-30 20:31:55 +0800

    支持 level 属性

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -7,7 +7,7 @@
     <Button theme="link">你好</Button>
     <Button theme="text">你好</Button>
   </div>
-  <h1>示例1</h1>
+  <h1>示例2</h1>
   <div>
     <Button size="big">大按钮</Button>
     <Button>中按钮</Button>
@@ -22,6 +22,24 @@
     <Button size="big" theme="text">大按钮</Button>
     <Button theme="text">中按钮</Button>
     <Button size="small" theme="text">小按钮</Button>
+  </div>
+  <h1>示例3</h1>
+  <div>
+    <dir>
+      <Button level="main">主要按钮</Button>
+      <Button>普通按钮</Button>
+      <Button level="danger">危险按钮</Button>
+    </dir>
+    <dir>
+      <Button theme="link" level="main">主要链接按钮</Button>
+      <Button theme="link">普通链接按钮</Button>
+      <Button theme="link" level="danger">危险链接按钮</Button>
+    </dir>
+    <dir>
+      <Button theme="text" level="main">主要文字按钮</Button>
+      <Button theme="text">普通文字按钮</Button>
+      <Button theme="text" level="danger">危险文字按钮</Button>
+    </dir>
   </div>
 </template>
 
