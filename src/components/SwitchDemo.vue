3c725975e1b88810b3297b7ea5e7a64226299ee9
tang <923233700@qq.com>
2020-10-30 11:08:53 +0800

    可以接受 value input 事件

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,18 +1,22 @@
 <template>
   <div>
-    <Switch />
+    <Switch :value="y" @input="y = $event" />
   </div>
 </template>
 
 <script>
+import { ref } from "vue";
 import Switch from "../lib/Switch.vue";
 export default {
   components: {
     Switch,
   },
+  setup() {
+    const y = ref(false);
+    return { y };
+  },
 };
 </script>
 
 <style lang="scss" scoped>
-
 </style>
