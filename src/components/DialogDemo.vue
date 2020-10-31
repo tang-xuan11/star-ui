97e79706f30a538586f9acfb706ffc5c090dfbec
tang <923233700@qq.com>
2020-10-31 10:49:00 +0800

    创建 Dialog 组件

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,9 +1,14 @@
 <template>
   <div>Dialog 的文档</div>
+  <h1>示例1</h1>
+  <Dialog></Dialog>
 </template>
 
 <script>
-export default {};
+import Dialog from "../lib/Dialog.vue";
+export default {
+  components: { Dialog },
+};
 </script>
 
 <style lang="scss" scoped>
