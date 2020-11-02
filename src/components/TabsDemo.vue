db7ad919f257b48fd097413e00665a0e2aae7c5f
tang <923233700@qq.com>
2020-11-02 20:24:22 +0800

    检查 Tabs 的子标签必须是 Tab

--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -1,9 +1,20 @@
 <template>
   <div>Tabs 的文档</div>
+  <Tabs>
+    <Tab title="导航1">内容1</Tab>
+    <Tab title="导航2">内容2</Tab>
+  </Tabs>
 </template>
 
 <script>
-export default {};
+import Tab from "../lib/Tab.vue";
+import Tabs from "../lib/Tabs.vue";
+export default {
+  components: {
+    Tab,
+    Tabs,
+  },
+};
 </script>
 
 <style lang="scss" scoped>
---
+++ src/lib/Tab.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>Tab 组件</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" >
+</style>
