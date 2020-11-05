0853722e9f3eb75911e0ce62a9c56a23a829ccb7
tang <923233700@qq.com>
2020-11-05 09:53:11 +0800

    填充文档内容

--- src/view/Intro.vue
+++ src/view/Intro.vue
@@ -1,5 +1,11 @@
 <template>
-  <div>Intro</div>
+  <article>
+    <h1>介绍</h1>
+    <p>Star UI 是一款基于 Vue 3 和 TypeScript 的 UI 组件库。</p>
+    <p>这款组件库是为了学习Vue3而编写的，强烈不建议你将此 UI 库用于生产环境</p>
+
+    下一节：<a href="#/doc/install">安装</a>
+  </article>
 </template>
 
 <script>
