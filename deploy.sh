6f753cc9d5b63ca8468447ca7e4c2159ee3f68bd
tang <923233700@qq.com>
2020-11-07 20:30:07 +0800

    修改 deploy 脚本

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,6 @@ git add . &&
 git commit -m "update" &&
 git branch -M master &&
 git remote add origin git@github.com:tang-xuan11/star-ui-website.git &&
-git push -f -u origin main &&
+git push -f -u origin master &&
 cd -
 echo https://tangyinxuan.top/star-ui-website/
